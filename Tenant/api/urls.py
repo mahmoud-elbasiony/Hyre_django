c431845a12aae3141e028db74ad9ae70273ecf69
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-06-14 23:40:22 +0300

    add urls for candidate in api urls

--- Tenant/api/urls.py
+++ Tenant/api/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import path, include
 from Tenant.api.views import InterviewView, InterviewDetailView
+from Tenant.api.views.candidates import CandidateView
 
 urlpatterns = [
     path('interviews', InterviewView.as_view()),
-    path('interviews/<str:pk>', InterviewDetailView.as_view())
+    path('interviews/<str:pk>', InterviewDetailView.as_view()),
+    path('candidates', CandidateView.as_view()),
+    path('candidates/<str:pk>', CandidateView.as_view())
 
 ]
