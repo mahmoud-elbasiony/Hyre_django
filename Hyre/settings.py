020c7a7f9041d83b7244721752b49b9c34205102
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-06-14 20:54:41 +0300

    update setting to add email conf

--- Hyre/settings.py
+++ Hyre/settings.py
@@ -31,7 +31,7 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-# EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
