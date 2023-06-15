62bf21dd7574df3b93d3892e454f690fce75933a
Yousef Adel <yousefjooo2018@gmail.com>
2023-06-16 01:05:16 +0300

    make postion serializer

--- Tenant/api/serializers.py
+++ Tenant/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from Tenant.models import Interview
+from Tenant.models import Interview,Position
 
 
 class InterviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Interview
+        fields = '__all__'
+
+
+class PositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Position
         fields = '__all__'
