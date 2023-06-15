dedc58787259c45729be2cba2ab4987d6c76a264
omaralaa <omaralaa0989@gmail.com>
2023-06-15 16:12:39 +0300

    add task in Schedular app

--- Scheduler/tasks.py
+++ Scheduler/tasks.py
@@ -22,3 +22,34 @@ console_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - 
 console_handler.setFormatter(console_formatter)
 logger.addHandler(console_handler)
 
+@shared_task
+def check_interview():
+   response_data=[]
+   interviews = Interview.objects.all()
+   serializers = InterviewSerializer(interviews, many=True)
+   for interview in serializers.data:
+      interview_date_str = interview["date"]
+      interview_date_time = datetime.strptime(interview_date_str, '%Y-%m-%d').date()
+      days_left = (interview_date_time - date.today()).days
+    #   print(days_left)
+      if days_left < 10:
+            response_data.append(interview)
+   if len(response_data)>0:    
+    logger.info(response_data) 
+    print(response_data)
+    return Response({
+                "success": True,
+                "message": "interviews retreived successfully",
+                "data": serializers.data
+            }, status=status.HTTP_200_OK) 
+   else:
+      return Response({
+            "success": True,
+            "message": "no interviews within 10 days",
+        }, status=status.HTTP_200_OK) 
+      
+
+
+
+
+ 
