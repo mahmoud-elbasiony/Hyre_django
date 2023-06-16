0c6f63332e1ca4778c7878a9469fc7e33938b093
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-06-16 12:06:37 +0300

    Applicant urls

--- Tenant/api/urls.py
+++ Tenant/api/urls.py
@@ -1,9 +1,14 @@
-
 from django.urls import path, include
 from Tenant.api.views import InterviewView, InterviewDetailView
+from Tenant.api.views.applicant import index, show, store, edit, destroy
 
 urlpatterns = [
-    path('interviews', InterviewView.as_view()),
-    path('interviews/<str:pk>', InterviewDetailView.as_view())
-
+    path('interviews/', InterviewView.as_view() ),
+    path('interviews/<str:pk>/', InterviewDetailView.as_view() ),
+    path('applicants/create/', store),
+    path('applicants/', index),
+    path('applicants/<str:pk>/edit/', edit),
+    path('applicants/<str:pk>/destroy/', destroy),
+    path('applicants/<str:pk>/', show)
 ]
+
