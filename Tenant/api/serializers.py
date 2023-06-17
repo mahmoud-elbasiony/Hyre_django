209404705c69056b97c64e5c01893e8374c84181
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-06-17 20:54:12 +0300

    modify InterviewSerializer

--- Tenant/api/serializers.py
+++ Tenant/api/serializers.py
@@ -1,24 +1,28 @@
 from rest_framework import serializers
-from Tenant.models import Interview,Position, Applicant
-
+from Tenant.models import Interview, Position, Applicant
 
 
 class InterviewSerializer(serializers.ModelSerializer):
+    company = serializers.StringRelatedField()
+    interviewer = serializers.StringRelatedField()
+    applicant = serializers.StringRelatedField()
+    position = serializers.StringRelatedField()
+
     class Meta:
         model = Interview
         fields = '__all__'
 
 
 class ApplicantSerializer(serializers.ModelSerializer):
-    position = serializers.StringRelatedField() # used to return string name instead of id of position like accessors in laravel
+    # used to return string name instead of id of position like accessors in laravel
+    position = serializers.StringRelatedField()
+
     class Meta:
         model = Applicant
         fields = '__all__'
+
 
 class PositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Position
         fields = '__all__'
-
-
-
