83a9723b67b5c6b151df9ff06879fb87e31d4879
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-06-18 21:49:34 +0300

    company image view

---
+++ cloudinary_app/api/views/image.py
@@ -0,0 +1,34 @@
+from rest_framework.response import Response
+from rest_framework import status, generics
+from Landlord.models import Tenant
+from cloudinary_app.api.serializers import ImageSerializer
+
+class ImageView(generics.GenericAPIView):
+    serializer_class = ImageSerializer
+
+    def put(self, request):
+        company = request.user.company_id
+        company = Tenant.objects.get(id=company)
+        try:
+            serializer = self.serializer_class(company, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    "success": True,
+                    "message": "Image uploaded successfully",
+                    "data": serializer.data
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    "success": False,
+                    "message": serializer.errors
+                }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(e)
+            return Response({
+                "success": False,
+                "message": "Server error",
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
