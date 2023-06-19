7ac633934d3ca7be276638a7c751314453db3389
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-06-19 21:39:23 +0300

    retrieve the price id from db depends on client data sent

--- Stripe/views.py
+++ Stripe/views.py
@@ -3,21 +3,25 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 import stripe
+from Landlord.models import Subscription # gives me Import "Landlord.models" could not be resolved
 
 @api_view(['POST'])
 def create_checkout_session(request):
+
     try:
+        subscription_data = Subscription.objects.get(name=request.data['name'],type=request.data['type'])
+        price_id = subscription_data.pr_id
         stripe.api_key = settings.SECRET_STRIPE_KEY
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=['card'],
             line_items=[
                 {
-                    'price': "price_1NKj05IfGZb04ICqEBCfWQZP",
+                    'price': price_id,
                     'quantity': 1,
                 },
             ],
             mode='payment',
-            success_url='http://localhost:4200/dashboard',
+            success_url='http://localhost:4200/dashboard/payment/successful',
             cancel_url='http://localhost:4200/dashboard/subscriptions',
         )
     except stripe.error.StripeError as e:
