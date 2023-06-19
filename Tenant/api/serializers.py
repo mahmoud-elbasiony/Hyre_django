ce57bc2e8591984a83152665f65607e72c6dae2c
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-06-19 16:17:50 +0300

    Company view for applicants

--- Tenant/api/serializers.py
+++ Tenant/api/serializers.py
@@ -1,27 +1,35 @@
 from rest_framework import serializers
-from Tenant.models import Interview,Position, Applicant
 
+from Tenant.models import Interview,Position, Applicant,User
 
 
 class InterviewSerializer(serializers.ModelSerializer):
+    company = serializers.StringRelatedField()
+    interviewer = serializers.StringRelatedField()
+    applicant = serializers.StringRelatedField()
+    position = serializers.StringRelatedField()
+
     class Meta:
         model = Interview
         fields = '__all__'
 
 
 class ApplicantSerializer(serializers.ModelSerializer):
+    # used to return string name instead of id of position like accessors in laravel
+    position = serializers.StringRelatedField()
+
     class Meta:
         model = Applicant
         fields = '__all__'
+
 
 class PositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Position
         fields = '__all__'
 
-# class CandidatesSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Applicant
-#         fields = '__all__'
 
-
+class GetUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Interview
+        fields = ["name","email","company","username"]
