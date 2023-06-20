78177097ff4dc443311cbbbed6e60559b74be4af
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-06-20 22:38:10 +0300

    Adding date to the token

--- Tenant/api/token.py
+++ Tenant/api/token.py
@@ -1,17 +1,25 @@
-import os
-import jwt
 import time
-from jwt.exceptions import InvalidTokenError
+import jwt
+import os
 
 def createToken(company_id):
-    expiration_time = 60 # will be fetched from the position table
+    expiration_time = 60  # will be fetched from the position table
+    current_time = time.time()
+    expiration_timestamp = current_time + expiration_time
+
     payload = {
         'company_id': company_id,
-        'exp':  time.time() + expiration_time
+        'exp': expiration_timestamp
     }
+
     secret = os.getenv('GENERATE_LINK_SECRET')
-    token = jwt.encode(payload, secret, algorithm='HS256')  # 1 hour expiration
-    return token
+    token = jwt.encode(payload, secret, algorithm='HS256')
+
+    return {
+        "token": token,
+        "expiration_date": time.strftime("%H:%M %d-%m", time.localtime(expiration_timestamp))
+    }
+
 
 def verifyToken(token):
     try:
