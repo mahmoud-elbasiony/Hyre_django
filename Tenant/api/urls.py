8d9c2d6a8570d15e4a8d3c6751e535ee56cd9379
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-06-20 20:43:50 +0300

    Adding middlewares

--- Tenant/api/urls.py
+++ Tenant/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from Tenant.api.views import InterviewView, InterviewDetailView
-from Tenant.api.views.applicant import index, show, store, edit, destroy, generateAplicantFormLink, show_by_position
+from Tenant.api.views.applicant import index, show, store, destroy, generateAplicantFormLink, show_by_position
 from Tenant.api.views.candidates import CandidateView
 from .views.mail import MailView
 from .views.position import PositionView,PositionDetailView
@@ -14,7 +14,6 @@ urlpatterns = [
     path('applicants/', index),
     path('users', UserView.as_view()),
     path('applicants/position/<str:position_id>' , show_by_position),
-    path('applicants/<str:pk>/edit/', edit),
     path('applicants/<str:pk>/destroy/', destroy),
     path('applicants/<str:pk>/', show),
     path('positions', PositionView.as_view()),
